24a06911ebfa84759acb94b1d7be128279314abe
Nicolas Talfer <nicolas.talfer@myriadgroup.com>
2023-12-08 23:21:09 +0100

    d8 p2

--- lib/d8.ex
+++ lib/d8.ex
@@ -1,5 +1,10 @@
 defmodule D8 do
   def p1(file) do
+    {lr, lines} = parse_file(file)
+    traverse(lr, lr, lines, "AAA", 0)
+  end
+
+  defp parse_file(file) do
     [lr | lines] =
       file
       |> File.read!()
@@ -15,22 +20,58 @@ defmodule D8 do
 
     lr = to_charlist(lr)
 
-    steps = 0
-
-    traverse(lr, lr, lines, "AAA", steps)
+    {lr, lines}
   end
 
   defp traverse(_, _, _, "ZZZ", steps) do
     steps
   end
+
   defp traverse(lr, [], lines, current, steps) do
     traverse(lr, lr, lines, current, steps)
   end
+
   defp traverse(lr, [dir | dirs], lines, current, steps) do
     {^current, left, right} = List.keyfind(lines, current, 0)
+
     case dir do
-      ?L -> traverse(lr, dirs, lines, left, steps+1)
-      ?R -> traverse(lr, dirs, lines, right, steps+1)
+      ?L -> traverse(lr, dirs, lines, left, steps + 1)
+      ?R -> traverse(lr, dirs, lines, right, steps + 1)
     end
+  end
+
+  def p2(file) do
+    {lr, lines} = parse_file(file)
+
+    lines
+    |> Enum.map(fn {current, _, _} -> current end)
+    |> Enum.filter(&String.ends_with?(&1, "A"))
+    |> Enum.map(fn start -> traverse_p2(lr, lr, lines, start, 0) end)
+    |> lcm()
+  end
+
+  defp traverse_p2(_, _, _, <<_, _, ?Z>>, steps) do
+    steps
+  end
+
+  defp traverse_p2(lr, [], lines, current, steps) do
+    traverse_p2(lr, lr, lines, current, steps)
+  end
+
+  defp traverse_p2(lr, [dir | dirs], lines, current, steps) do
+    {^current, left, right} = List.keyfind(lines, current, 0)
+
+    case dir do
+      ?L -> traverse_p2(lr, dirs, lines, left, steps + 1)
+      ?R -> traverse_p2(lr, dirs, lines, right, steps + 1)
+    end
+  end
+
+  defp lcm(a, b) do
+    div(a * b, Integer.gcd(a, b))
+  end
+
+  defp lcm(list) do
+    Enum.reduce(list, &lcm(&2, &1))
   end
 end
