617782d4f5916ff7249cba6a163d216e84674474
Nicolas Talfer <nicolas.talfer@myriadgroup.com>
2023-12-08 22:28:27 +0100

    d7 p2

--- lib/d7.ex
+++ lib/d7.ex
@@ -1,5 +1,6 @@
 defmodule D7 do
-  def p1(file) do
+  #def p1(file) do
+    def p2(file) do
     hbs =
       file
       |> File.read!()
@@ -36,8 +37,28 @@ defmodule D7 do
   defp get_type([{_, 5}]) do
     @five_of_a_kind
   end
+#################
+  defp get_type([{_, 4}, {?J, _}]) do
+    @five_of_a_kind
+  end
 
-  defp get_type([{_, 4} | _]) do
+  defp get_type([{?J, 4}, _]) do
+    @five_of_a_kind
+  end
+
+  defp get_type([{_, 4}, _]) do
+    @four_of_a_kind
+  end
+#############
+  defp get_type([{_, 3}, {?J, 2}]) do
+    @five_of_a_kind
+  end
+
+  defp get_type([{?J, 3}, {_, 2}]) do
+    @five_of_a_kind
+  end
+
+  defp get_type([{?J, 3}, _, _]) do
     @four_of_a_kind
   end
 
@@ -45,23 +66,53 @@ defmodule D7 do
     @full_house
   end
 
-  defp get_type([{_, 3} | _]) do
+  defp get_type([{_, 3}, {letter1, 1}, {letter2, 1}]) when letter1 == ?J or letter2 == ?J do
+    @four_of_a_kind
+  end
+
+  defp get_type([{_, 3}, _, _]) do
+    @three_of_a_kind
+  end
+################
+  defp get_type([{_, 2}, {_, 2}, {?J, 1}]) do
+    @full_house
+  end
+
+  defp get_type([{letter1, 2}, {letter2, 2}, _]) when letter1 == ?J or letter2 == ?J do
+    @four_of_a_kind
+  end
+
+  defp get_type([{?J, 2}, _, _, _]) do
     @three_of_a_kind
   end
 
-  defp get_type([{_, 2}, {_, 2} | _]) do
+  defp get_type([{_, 2}, {_, 2}, _]) do
     @two_pairs
   end
 
-  defp get_type([{_, 2} | _]) do
+  defp get_type([{_, 2}, {letter1, 1}, {letter2, 1}, {letter3, 1}]) when letter1 == ?J or letter2 == ?J or letter3 == ?J do
+    @three_of_a_kind
+  end
+
+  defp get_type([{_, 2}, _, _, _]) do
     @one_pair
   end
 
-  defp get_type(_) do
-    @high_hand
+  defp get_type([{l1, 1}, {l2, 1}, {l3, 1}, {l4, 1}, {l5, 1}]) do
+
+    case Enum.any?([l1,l2,l3,l4,l5] , fn l -> l == ?J end) do
+      true ->
+        @one_pair
+      false ->
+        @high_hand
+    end
   end
 
-  def compare_hbs(%{type: type1, hand: hand1} = hb1, %{type: type2, hand: hand2} = hb2) do
+  # defp get_type(hand) do
+  #   @high_hand
+  # end
+
+  def compare_hbs(%{type: type1, hand: hand1}, %{type: type2, hand: hand2}) do
     case type1 == type2 do
       true ->
         compare_hands(to_charlist(hand1), to_charlist(hand2))
@@ -81,6 +132,6 @@ defmodule D7 do
   defp strength(?A), do: 300
   defp strength(?K), do: 299
   defp strength(?Q), do: 298
-  defp strength(?J), do: 297
+  defp strength(?J), do: 0 #297
   defp strength(?T), do: 296
 end
