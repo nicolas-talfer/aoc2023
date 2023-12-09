61949a04bcc4f650f1cfaa1d2ad53517d6eb6944
Nicolas Talfer <nicolas.talfer@myriadgroup.com>
2023-12-09 14:20:59 +0100

    d9 p2

--- lib/d9.ex
+++ lib/d9.ex
@@ -1,5 +1,6 @@
 defmodule D9 do
-  def p1(file) do
+ # def p1(file) do
+    def p2(file) do
     file
     |> File.read!()
     |> String.split("\n")
@@ -13,17 +14,21 @@ defmodule D9 do
   end
 
   defp traverse(line) do
-    traverse(line, [List.last(line)])
-    |> Enum.reduce(fn x, acc -> x + acc end)
+    traverse(line, [List.first(line)])
+    |> Enum.reduce(fn x, acc -> x - acc end)
+    #traverse(line, [List.last(line)])
+    #|> Enum.reduce(fn x, acc -> x + acc end)
   end
 
   defp traverse(line, acc) do
     next_line = do_traverse_once(line, [])
     case Enum.all?(next_line, &(&1 == 0)) do
       true ->
-        [List.last(next_line) | acc]
+        #[List.last(next_line) | acc]
+        [List.first(next_line) | acc]
       false ->
-        traverse(next_line, [List.last(next_line) | acc])
+        #traverse(next_line, [List.last(next_line) | acc])
+        traverse(next_line, [List.first(next_line) | acc])
     end
   end
 
